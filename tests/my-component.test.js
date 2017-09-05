d2e269c23ab4230e1f4f4245a16f689afeb16f8d
Rafael Minguet <rminguet@gmail.com>
2017-09-05 08:29:10 +0200

    Initial commit

---
+++ tests/my-component.test.js
@@ -0,0 +1,13 @@
+import test from 'tape';
+
+// Component to test
+const FizzBuzz = function () {
+
+
+    return {};
+};
+
+// Testear que devuelve el primer número
+test('-------- Component: returning the first element...', (assert) => {
+    
+});
